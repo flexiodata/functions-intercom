f55a15624980e9cb8dd2970766d5684feccbc19d
Aaron Williams <aaron@flex.io>
2019-10-29 14:31:44 -0500

    use integration connection info

--- intercom-list-events.py
+++ intercom-list-events.py
@@ -39,9 +39,7 @@ from collections import OrderedDict
 def flexio_handler(flex):
 
     # get the api key from the variable input
-    auth_token = flex.connections['intercom'].get_credentials()
-    auth_token = auth_token['access_token']
-    #auth_token = dict(flex.vars).get('intercom_connection')
+    auth_token = dict(flex.vars).get('intercom_connection')
     if auth_token is None:
         flex.output.content_type = "application/json"
         flex.output.write([[""]])
